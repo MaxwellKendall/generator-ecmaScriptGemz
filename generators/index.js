c663cceeb88569412cb904c9ac5622bd06be00db
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-08-18 10:24:53 -0400

    init

---
+++ generators/index.js
@@ -0,0 +1,14 @@
+const Generator = require('yeoman-generator');
+
+class extends Generator {
+  // The name `constructor` is important here
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    // Next, add your custom code
+    this.option('babel'); // This method adds support for a `--babel` flag
+  }
+}
+
+module.exports = Generator;
